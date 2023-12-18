43de36f3470db6c05f8f18e3c30d77c53d3ba81c
MadlyJado <jadonchenard@gmail.com>
2023-12-18 18:32:06 -0500

    first commit

---
+++ macchiato.lua
@@ -0,0 +1,107 @@
+local macchiato = {}
+
+function macchiato.rosewater()
+    return "#f4dbd6"
+end
+
+function macchiato.flamingo()
+    return "#f0c6c6"
+end
+
+function macchiato.pink()
+    return "#f5bde6"
+end
+
+function macchiato.mauve()
+    return "#c6a0f6"
+end
+
+function macchiato.red()
+    return "#ed8796"
+end
+
+function macchiato.maroon()
+    return "#ee99a0"
+end
+
+function macchiato.peach()
+    return "#f5a97f"
+end
+
+function macchiato.yellow()
+    return "#eed49f"
+end
+
+function macchiato.green()
+    return "#a6da95"
+end
+
+function macchiato.teal()
+    return "#8bd5ca"
+end
+
+function macchiato.sky()
+    return "#91d7e3"
+end
+
+function macchiato.sapphire()
+    return "#7dc4e4"
+end
+
+function macchiato.blue()
+    return "#8aadf4"
+end
+
+function macchiato.lavender()
+    return "#b7bdf8"
+end
+
+function macchiato.text()
+    return "#cad3f5"
+end
+
+function macchiato.subtext1()
+    return "#b8c0e0"
+end
+
+function macchiato.subtext0()
+    return "#a5adcb"
+end
+
+function macchiato.overlay2()
+    return "#939ab7"
+end
+
+function macchiato.overlay1()
+    return "#8087a2"
+end
+
+function macchiato.overlay0()
+    return "#6e738d"
+end
+
+function macchiato.surface2()
+    return "#5b6078"
+end
+
+function macchiato.surface1()
+    return "#494d64"
+end
+
+function macchiato.surface0()
+    return "#363a4f"
+end
+
+function macchiato.base()
+    return "#24273a"
+end
+
+function macchiato.mantle()
+    return "#1e2030"
+end
+
+function macchiato.crust()
+    return "#181926"
+end
+
+return macchiato
